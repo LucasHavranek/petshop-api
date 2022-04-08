ec5d915f44379467802028c554c9385a85977c57
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-04-08 17:37:38 -0300

    Nova instância banco de dados

--- repositories/database.js
+++ repositories/database.js
@@ -1,7 +1,7 @@
 import sequelize from 'sequelize'
 
 const sequelizeConnection = new sequelize(
-    "postgres://eaxygmcu:RCoIUVBYY3SUCwLzEXEQFnKD_K7cfeCO@kesavan.db.elephantsql.com/eaxygmcu", {
+    "postgres://ptimcwmn:TE7ToLUGX-Q_3GPYABs_Hy1f9pRvAiK6@kesavan.db.elephantsql.com/ptimcwmn", {
         dialect: 'postgress',
         define: {
             timestamps: false
